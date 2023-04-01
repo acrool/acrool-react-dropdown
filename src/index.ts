0df4dd04b5f2c1ef5d77b16331ef2ce3ac92b9c7
imagine10255 <imagine10255@gmail.com>
2023-04-02 00:27:26 +0800

    empty project

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export {default as Dropdown} from './Dropdown/Dropdown';
-export {default as DropdownMulti} from './Dropdown/DropdownMulti';
-export {default as DropdownElClassNames} from './Dropdown/el-class-names';
-export * from './Dropdown/utils';
-
-export * from './Dropdown/types';
